d3921255a1bcde2ab3dca26c6e649cedf9b49037
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-04-07 20:31:46 -0700

    added parties tables, updated schema and seeds sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,13 @@
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+/*
+we used a TEXT data type for description instead of VARCHAR. A party's description has the potential to be anywhere from one to several sentences long, but the VARCHAR data type must declare a limit on the length. TEXT, on the other hand, can store much longer strings of varying length.
+*/
+
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
