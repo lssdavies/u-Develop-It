a8351f254d0fe224e730383201a8edeaa3f2037d
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-04-07 23:03:33 -0700

    added ref notes

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -50,3 +50,29 @@ router.get("/votes", (req, res) => {
 });
 
 module.exports = router;
+
+// SQL provides a number of aggregate functions that can perform various math-related logic on your data. In the case of COUNT(), it will count how many times a certain field value appears.
+
+// Other useful aggregate functions in SQL include:
+
+// AVG() to return the average value within a group
+
+// SUM() to add up all of the values in a group
+
+// MIN() to return the minimum value of a group
+
+// When you run the SELECT COUNT(candidate_id) statement as-is, however, it simply counts up how many rows there are in the table, which isn't all that helpful. We want to count votes per candidate, not total votes cast.
+
+// This is where the GROUP BY clause comes into play. GROUP BY can consolidate several rows of data, grouping by a shared value (e.g., candidate_id). The nice thing about GROUP BY is that you can then run an aggregate function to retrieve an average, sum, or minimum value from the group.
+
+// The candidate_id field is a foreign key, so we can still join this table with the candidates table. While we're at it, we can also join the candidates table with the parties table to pull in their party affiliation.
+
+// By the looks of it, the JS Juggernauts Party is on fire! This data is essentially what we want, but there are a few more adjustments we can make. For one, we should rename the dynamic COUNT() field to something easier for the front-end team to use. Second, we might as well order these results by vote count so it's easier to see the winner at a glance.
+
+// Update the SQL query to look like this:
+
+// SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+// FROM votes
+// LEFT JOIN candidates ON votes.candidate_id = candidates.id
+// LEFT JOIN parties ON candidates.party_id = parties.id
+// GROUP BY candidate_id ORDER BY count DESC;
